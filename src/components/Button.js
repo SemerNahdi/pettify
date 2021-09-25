fe980609165bc8573c61fc1ea8cc9ef59265012e
Danny393 <danielacosta393@gmail.com>
2021-09-25 15:09:11 -0400

    button changes for more readability and felxability when creating styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,61 +5,48 @@ import {Button as NBButton, Text, Spinner} from "native-base";
 
 import {Theme} from "./Theme";
 
-import type {BaseProps} from "./Types";
-
-type ButtonProps = BaseProps & {
-    label: string,
-    primary?: boolean,
-    transparent?: boolean,
-    disabled?: boolean,
-    full?: boolean,
-    onPress: () => mixed,
-    loading?: boolean
-};
-
-export default class Button extends React.PureComponent<ButtonProps> {
+export default class Button extends React.Component<> {
     render(): React.Node {
-        const {label, full, primary, disabled, transparent, onPress, style, loading} = this.props;
-        const computedStyle = [styles.base];
-        if (primary && !transparent) {
-            computedStyle.push(styles.primary);
+        const {label, full, disabled, transparent, onPress, style, loading} = this.props;
+        
+        var appliedStyle
+        var primary = false
+
+        if(style === "primary")
+        {
+            appliedStyle = styles.primary
+            primary = true
         }
-        computedStyle.push(style);
+        else if(style === "diagnosis")
+        {
+            appliedStyle = styles.diagnosisButton
+        }
+        else
+        {
+            appliedStyle = styles.base
+        }
+
         return (
             <NBButton
                 title={label}
-                {...{
-                    full,
-                    primary,
-                    onPress,
-                    style: computedStyle,
-                    transparent: !primary || transparent,
-                    disabled
-                }}
+                style = { appliedStyle }
+                onPress = {onPress}
+                full = {full}
+                disabled = {disabled}
+                transparent = {!primary || transparent}
             >
-                {
-                    !!loading && (
-                        <Spinner color="white" />
-                    )
-                }
-                {
-                    !loading && (
-                        <Text
-                            style={[
-                                primary ? Theme.typography.large : Theme.typography.regular,
-                                // eslint-disable-next-line
-                                {
-                                    // eslint-disable-next-line
-                                    color: disabled ? "transparent" : (primary ? (transparent ? Theme.palette.primary : Theme.palette.black) : Theme.typography.color),
-                                    fontSize: primary ? 16 : Theme.typography.regular.fontSize,
-                                    fontFamily: Theme.typography.semibold
-                                }
-                            ]}
-                        >
-                            {label}
-                        </Text>
-                    )
-                }
+                {!!loading && (<Spinner color="white" />)}
+                {!loading &&(
+                <Text
+                    style={{
+                        color: disabled ? "transparent" : (primary ? (transparent ? Theme.palette.primary : Theme.palette.black) : Theme.typography.color),
+                        fontSize: primary ? 16 : Theme.typography.regular.fontSize,
+                        fontFamily: Theme.typography.semibold
+                    }}
+                >
+                    {label}
+                </Text>
+                )}
             </NBButton>
         );
     }
@@ -67,7 +54,6 @@ export default class Button extends React.PureComponent<ButtonProps> {
 
 const styles = StyleSheet.create({
     base: {
-        color: Theme.palette.black,
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "center",
@@ -75,7 +61,6 @@ const styles = StyleSheet.create({
         paddingHorizontal: 5
     },
     primary: {
-        color: Theme.palette.black,
         backgroundColor: Theme.palette.primary,
         shadowColor: "rgba(85, 85, 85, 0.29)",
         shadowOffset: { width: 0, height: 2 },
@@ -89,5 +74,25 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         paddingVertical: 10,
         paddingHorizontal: 5
+    },
+    diagnosisButton: {
+        backgroundColor: Theme.palette.primary,
+        shadowColor: "rgba(85, 85, 85, 0.29)",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 1,
+        shadowRadius: 7,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: Theme.palette.white,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 10,
+        paddingHorizontal: 5,
+        position: 'absolute',
+        bottom: -200,
+        zIndex: 0,
+        padding: 5,
+        alignSelf: "center"
     }
 });
