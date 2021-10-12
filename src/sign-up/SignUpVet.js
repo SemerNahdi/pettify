babf2f10cb13ac943c19fcee562ab0e24ff4aa24
OAKevin <67325994+OAKevin@users.noreply.github.com>
2021-10-12 17:55:34 -0400

    .

---
+++ src/sign-up/SignUpVet.js
@@ -0,0 +1,84 @@
+// @flow
+import autobind from "autobind-decorator";
+import * as React from "react";
+import {StyleSheet, View} from "react-native";
+
+import {TextField, Switch, Theme, Text} from "../components";
+import type {NavigationProps} from "../components/Types";
+
+import SignUpStore from "./SignUpStore";
+import SignUpContainer from "./SignUpContainer";
+
+type EmailState = {
+    email: string
+};
+
+export default class Email extends React.Component<NavigationProps<*>, EmailState> {
+
+    state = {
+        email: ""
+    };
+
+    @autobind
+    setEmail(email: string) {
+        this.setState({ email });
+    }
+    @autobind
+    setPassword(password: string) {
+        this.setState({ password });
+    }
+
+    @autobind
+    next() {
+        const {email} = this.state;
+        if (email === "") {
+            // eslint-disable-next-line
+            alert("Please provide an email.");
+        } else {
+            SignUpStore.email = email;
+            this.props.navigation.navigate("SignUpRole");
+        }
+    }
+
+    render(): React.Node {
+        const {navigation} = this.props;
+        return (
+            <SignUpContainer title="Vet Sign Up" subtitle="" next={this.next} {...{ navigation }}>
+                <TextField
+                    placeholder="Email"
+                    keyboardType="email-address"
+                    contrast
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    returnKeyType="go"
+                    onSubmitEditing={this.next}
+                    onChangeText={this.setEmail}
+                />
+                <TextField
+                    secureTextEntry= {passView}
+                    placeholder="Password"
+                    contrast
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    returnKeyType="go"
+                    onSubmitEditing={this.next}
+                    onChangeText={this.setPassword}
+                    style={styles.textInput}
+                />
+            </SignUpContainer>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    text: {
+        flexWrap: "wrap",
+        marginLeft: Theme.spacing.small,
+        flex: 1
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginVertical: Theme.spacing.tiny
+    }
+});
