e5788405540e4066bbf383751b81f7e98b8f4fa0
OAKevin <67325994+OAKevin@users.noreply.github.com>
2021-10-25 19:02:27 -0400

    added roles to the Patients tab to check for the Admin

--- src/vet/patients/Patients.js
+++ src/vet/patients/Patients.js
@@ -4,22 +4,23 @@ import PatientItem from './PatientItem'
 import _ from 'lodash';
 import Pagination from 'react-native-pagination';//{Icon,Dot} also available
 import Firebase from "../../components/Firebase";
-import { NavHeaderWithButton, Theme } from "../../components";
+import { NavHeaderWithButton, Theme, NavHeader } from "../../components";
 import autobind from 'autobind-decorator';
-
 export default class Patients extends Component {
     @autobind
     buttonFn() {
       navigation.navigate("SignUpVet");
     }
   constructor(props){
-     super(props);
+      super(props);
       this.state = {
+        role: "",
         items: [],
         loading: true,
       };
-
       navigation = this.props.navigation;
+
+      
 
       this.retrieveFireStorePets();
     }
@@ -32,6 +33,10 @@ export default class Patients extends Component {
     //.where("role", "==", "p")
     .get()
     .then(docs => {
+      this.setState({
+        role: docs.data().role,
+      });
+
         var i = 0;
         docs.forEach(doc => {
             allUsers.push(doc.data());
@@ -71,7 +76,8 @@ export default class Patients extends Component {
 
   // REQUIRED for ReactNativePagination to work correctly
   onViewableItemsChanged = ({ viewableItems, changed }) =>this.setState({viewableItems})
-
+  
+  
   render() {
     if(this.state.loading)
     {
@@ -86,34 +92,82 @@ export default class Patients extends Component {
         </SafeAreaView>
         )
     }
-    return (
-      <View style={[styles.container]}>
-      <NavHeaderWithButton title="Users" buttonFn={this.buttonFn} buttonIcon="plus" />
+    // @inject("profileStore")
+    // Firebase.auth.onAuthStateChanged((user) => {
+    //   LogBox.ignoreAllLogs();
+    //   const { navigation, profileStore } = this.props;
+    //   await Loading.loadStaticResources();
+    //   Firebase.init();
+    //   const isUserAuthenticated = !!user;
+    //   const { uid } = Firebase.auth.currentUser;
+    // profileStore.init();
+    //var data;
+    
+            
+            if(role == "a")
+            {
+              return (
+                <View style={[styles.container]}>
+                
+                <NavHeaderWithButton title="Users" buttonFn={this.buttonFn} buttonIcon="plus" />
+                
+                    <FlatList
+                      data={this.state.items}
+                      ref={r=>this.refs=r}//create refrence point to enable scrolling
+                      keyExtractor={this._keyExtractor}//map your keys to whatever unique ids the have (mine is a "id" prop)
+                      renderItem={this._renderItem}//render each item
+                      onViewableItemsChanged={this.onViewableItemsChanged}//need this
+                    />
+                    <Pagination
+                      // remove this to get rid of dots next to list
+                      // dotThemeLight //<--use with backgroundColor:"grey"
+                      listRef={this.refs}//to allow React Native Pagination to scroll to item when clicked  (so add "ref={r=>this.refs=r}" to your list)
+                      paginationVisibleItems={this.state.viewableItems}//needs to track what the user sees
+                      paginationItems={this.state.items}//pass the same list as data
+                      paginationItemPadSize={0} //num of items to pad above and below your visable items
+                      startDotIconHide
+                      endDotIconHide
+                      dotTextHide
+                      dotIconHide
+                    />
+                  </View>
+                )
+            }else{
+              return (
+                <View style={[styles.container]}>
 
-        
-          <FlatList
-            data={this.state.items}
-            ref={r=>this.refs=r}//create refrence point to enable scrolling
-            keyExtractor={this._keyExtractor}//map your keys to whatever unique ids the have (mine is a "id" prop)
-            renderItem={this._renderItem}//render each item
-            onViewableItemsChanged={this.onViewableItemsChanged}//need this
-          />
-          <Pagination
-            // remove this to get rid of dots next to list
-            // dotThemeLight //<--use with backgroundColor:"grey"
-            listRef={this.refs}//to allow React Native Pagination to scroll to item when clicked  (so add "ref={r=>this.refs=r}" to your list)
-            paginationVisibleItems={this.state.viewableItems}//needs to track what the user sees
-            paginationItems={this.state.items}//pass the same list as data
-            paginationItemPadSize={0} //num of items to pad above and below your visable items
-            startDotIconHide
-            endDotIconHide
-            dotTextHide
-            dotIconHide
-          />
-        </View>
-      )
-  }
+                  <NavHeader title="Users"{...{ navigation }}/>
+              
+                  
+                    <FlatList
+                      data={this.state.items}
+                      ref={r=>this.refs=r}//create refrence point to enable scrolling
+                      keyExtractor={this._keyExtractor}//map your keys to whatever unique ids the have (mine is a "id" prop)
+                      renderItem={this._renderItem}//render each item
+                      onViewableItemsChanged={this.onViewableItemsChanged}//need this
+                    />
+                    <Pagination
+                      // remove this to get rid of dots next to list
+                      // dotThemeLight //<--use with backgroundColor:"grey"
+                      listRef={this.refs}//to allow React Native Pagination to scroll to item when clicked  (so add "ref={r=>this.refs=r}" to your list)
+                      paginationVisibleItems={this.state.viewableItems}//needs to track what the user sees
+                      paginationItems={this.state.items}//pass the same list as data
+                      paginationItemPadSize={0} //num of items to pad above and below your visable items
+                      startDotIconHide
+                      endDotIconHide
+                      dotTextHide
+                      dotIconHide
+                    />
+                  </View>
+              )
+            }
+      
+    }
+      // });
 };
+    //)
+  
+//};
 
 const styles = StyleSheet.create({
   container: {
