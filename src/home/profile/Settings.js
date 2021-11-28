42883d3a940367101a02aeb834775d606cae3720
rlope265 <rlope265@fiu.edu>
2021-11-28 02:05:26 -0500

    Added email to settings screen, password requirement for email and loading screen after saving changes.

--- src/home/profile/Settings.js
+++ src/home/profile/Settings.js
@@ -1,11 +1,12 @@
 // @flow
 import autobind from "autobind-decorator";
 import * as React from "react";
-import { Alert, StyleSheet, View, TouchableWithoutFeedback, Image } from "react-native";
+import { Alert, StyleSheet, View, TouchableWithoutFeedback, Image, SafeAreaView, ActivityIndicator,  } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { Content } from "native-base";
 import { Feather as Icon } from "@expo/vector-icons";
 import * as Permissions from "expo-permissions";
+import * as firebase from "firebase";
 
 import {
     NavHeader,
@@ -31,6 +32,9 @@ export default class Settings extends React.Component {
             loading: false,
             hasCameraRollPermission: null,
             address: "",
+            email: "",
+            editable: false,
+            passwordFailure: false,
         };
 
         navigation = this.props.navigation;
@@ -39,6 +43,7 @@ export default class Settings extends React.Component {
         this.state.name = profile.name;
         this.state.pic = profile.pic ? profile.pic : profile.picture.uri;
         this.state.address = profile.address;
+        this.state.email = profile.email;
 
         Permissions.askAsync(Permissions.CAMERA_ROLL)
         .then((stat) => {
@@ -46,27 +51,88 @@ export default class Settings extends React.Component {
         })
     }
 
+    /* Dialog to request user password */
+    @autobind
+    passwordDialog() {
+        this.title = this.state.passwordFailure ? "Incorrect password": "Enter password";
+        this.message = (this.state.passwordFailure ? 
+            "Entered password is incorrect. Please try again":
+            "Enter your password to change your email"
+        );
+        if (this.state.editable == false) {
+            return Alert.prompt(
+                this.title,
+                this.message,
+                [
+                    {
+                    text: "Cancel",
+                    style: "cancel"
+                    },
+                    {
+                        text: "OK",
+                        onPress: password => {
+                            const user = Firebase.auth.currentUser;
+                            const credential = firebase.auth.EmailAuthProvider.credential(
+                                user.email, 
+                                password
+                            );
+                            user.reauthenticateWithCredential(credential).then(() => {
+                                this.setState({icon: "unlock", editable: true});
+                            }).catch((error) => {
+                                this.setState({ passwordFailure: true });
+                                this.passwordDialog();
+                            });
+                        }
+                    }
+                ],
+                "secure-text"
+            );
+        }
+    }
+
+    /* Function for waiting 0.5 sec to retrieve profile after saving changes */
+    @autobind
+    async savefn() {
+        await setTimeout(()=>{
+            this.props.navigation.state.params.onSubmit();
+            this.props.navigation.goBack()
+        }, 500);
+    }
+
     @autobind
     async save(): Promise<void> {
         const originalProfile = profile;
-        const { name, pic, address } = this.state;
-        const { uid } = Firebase.auth.currentUser;
+        const { name, pic, address, email } = this.state;
+        const user = Firebase.auth.currentUser;
 
         this.setState({ loading: true });
 
         try {
             if (name !== originalProfile.name) {
-                await Firebase.firestore.collection("users").doc(uid).update({ name })
-                .then(() => {
-                    this.props.navigation.state.params.onSubmit();
-                    this.props.navigation.goBack()
+                await Firebase.firestore.collection("users").doc(user.uid).update({ name })
+                .then(async() => {
+                    await this.savefn();
+                }).catch((e) => {
+                    console.log('uploading name error => ', e);
                 });
             }
             if (address !== originalProfile.address) {
-                await Firebase.firestore.collection("users").doc(uid).update({ address })
-                .then(() => {
-                    this.props.navigation.state.params.onSubmit();
-                    this.props.navigation.goBack()
+                await Firebase.firestore.collection("users").doc(user.uid).update({ address })
+                .then(async() => {
+                    await this.savefn();
+                }).catch((e) => {
+                    console.log('uploading address error => ', e);
+                });
+            }
+            if (email !== originalProfile.email) {
+                Firebase.firestore.collection("users").doc(user.uid).update({ email })
+                .then(async () => {
+                    user.updateEmail(email).catch((error) => {
+                        console.error("Error updating email in auth: ", error);
+                    });
+                    await this.savefn();
+                }).catch((e) => {
+                    console.log('uploading email error => ', e);
                 });
             }
             if (pic !== originalProfile.pic) {
@@ -80,11 +146,10 @@ export default class Settings extends React.Component {
                     .then((pic) => {
                         Firebase.firestore
                             .collection("users")
-                            .doc(uid)
+                            .doc(user.uid)
                             .update({pic})
-                            .then(() => {
-                                this.props.navigation.state.params.onSubmit();
-                                this.props.navigation.goBack()
+                            .then(async () => {
+                                await this.savefn();
                             })
                     });
                 }).catch((e) => {
@@ -147,6 +212,11 @@ export default class Settings extends React.Component {
         this.setState({ address });
     }
 
+    @autobind
+    setEmail(email: string) {
+        this.setState({ email });
+    }
+
     render(): React.Node {
         const { loading } = this.state;
         if (this.state.hasCameraRollPermission === null) {
@@ -158,6 +228,18 @@ export default class Settings extends React.Component {
         } 
         else if (this.state.hasCameraRollPermission === false) {
             return <EnableCameraRollPermission />;
+        }
+        else if(loading){
+            return(
+            <SafeAreaView style={[styles.container]}>
+            <View style={{
+                paddingTop: "40%",
+                justifyContent:"center",
+            }}>
+                <ActivityIndicator size="large" />
+            </View>
+            </SafeAreaView>
+            )
         }
         return (
             <View style={styles.container}>
@@ -186,6 +268,26 @@ export default class Settings extends React.Component {
                         onChangeText={this.setName}
                         style={styles.textField}
                     />
+                    <View style={styles.separator}/>
+                    <Text style={styles.header}>Email</Text>
+                    <View style={styles.emailSection}>
+                        <TextField
+                            placeholder="Email"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            returnKeyType="go"
+                            defaultValue={this.state.email}
+                            onSubmitEditing={this.save}
+                            onChangeText={this.setEmail}
+                            editable={this.state.editable}
+                            style={{
+                                marginLeft: 4,
+                                flex: 1,
+                                color: this.state.editable ? "black": "gray",
+                            }}
+                        />
+                        <Icon name={this.state.editable ? "unlock": "lock"} size={20} style={styles.icon} onPress={this.passwordDialog}/>
+                    </View>
                     <View style={styles.separator}/>
                     <Text style={styles.header}>Address</Text>
                     <TextField
@@ -269,5 +371,13 @@ const styles = StyleSheet.create({
     textField: {
         marginLeft: 4,
         width: "100%",
+    },
+    icon: {
+        marginRight: 8,
+        marginBottom: 4,
+    },
+    emailSection: {
+        flex: 1,
+        flexDirection: 'row',
     },
 });
